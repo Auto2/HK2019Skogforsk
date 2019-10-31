3118b5e7170af836b345c669b160e230e365cf6c
Skogforsk dator <Auto2KTH@gmail.com>
2019-10-31 14:45:00 +0100

    mäktigt

--- build/testrig/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/skogforsk/catkin_ws/src/testrig/include".split(';') if "/home/skogforsk/catkin_ws/src/testrig/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "roscpp;std_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "testrig"
-PROJECT_SPACE_DIR = "/home/skogforsk/catkin_ws/devel"
-PROJECT_VERSION = "1.0.0"
